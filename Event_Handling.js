db052e119210cc6f593411c1006499f11f80ec34
Prithvi Sen <113365172+PrithviS18@users.noreply.github.com>
2023-12-21 21:43:59 +0530

    Create Event_Handling.js

---
+++ Event_Handling.js
@@ -0,0 +1,10 @@
+// Events allow you to write Javascript code that reacts to
+// certain situationsT
+
+// There are two types of Event Handling
+// 1. Event handlers (Used directly in the HTML)
+// 2. Event listners (Used in the Javascript using get elements
+
+// Bubbling and capture
+// In Bubbling the inner most element is handled First
+// But in Capture the outer most element is handled First (by giving true value to listner('click', function(), true))
